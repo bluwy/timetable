1f601c1c83c882b5707bba3487c58d5d13d757ae
BjornLuG <bjornlu.dev@gmail.com>
2020-03-27 19:51:28 +0800

    Add vue-formulate

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './plugins/fontawesome'
+import './plugins/formulate'
 import './assets/tailwind.css'
 
 Vue.config.productionTip = false
