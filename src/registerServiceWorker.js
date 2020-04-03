ee6a23b1d337f84ca88939ae804b9bfb98286499
BjornLuG <bjornlu.dev@gmail.com>
2020-04-03 12:32:35 +0800

    Show refresh window for new content

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,8 +19,14 @@ if (process.env.NODE_ENV === 'production') {
     updatefound() {
       console.log('New content is downloading.')
     },
-    updated() {
+    updated(registration) {
       console.log('New content is available; please refresh.')
+
+      if (window.confirm('New content available. Refresh app?')) {
+        const worker = registration.waiting
+        worker.postMessage({ action: 'SKIP_WAITING' })
+        window.location.reload()
+      }
     },
     offline() {
       console.log(
